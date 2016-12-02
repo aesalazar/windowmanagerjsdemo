814dc38ce91dd5c8a7102cdca4a3d97aeffb6e46
Ernie Salazar <salazare@verizon.net>
2016-12-02 17:42:49 -0500

    Added header and window controls to child window.

--- public/js/child.js
+++ public/js/child.js
@@ -1,3 +1,16 @@
-function closeWindow(){
-    windowfactory.Window.current.close();
+function minimizeWindow() {
+    windowfactory.Window.getCurrent().minimize();
+}
+
+function maximizeWindow() {
+    var win = windowfactory.Window.getCurrent();
+    
+    if (win.isMaximized())
+        win.restore();
+    else
+        win.maximize();
+}
+
+function closeWindow() {
+    windowfactory.Window.getCurrent().close();
 }
