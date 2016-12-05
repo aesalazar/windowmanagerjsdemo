9a86d6c79dee15dbf109ebb5323a7686633aa64d
Ernie Salazar <salazare@verizon.net>
2016-12-05 09:36:47 -0500

    Created openFinJson.js to provide json string via the routes.

---
+++ server/openFinJson.js
@@ -0,0 +1,27 @@
+function createJson(host){
+    const json = {
+        "devtools_port": 9090,
+        "startup_app": {
+            "name": "Node Performance Measure",
+            "url": "http://" + host + "/",
+            "uuid": "NodePerfMeasure",
+            "applicationIcon": "http://" + host + "/images/eikos-logo-multi.ico",
+            "autoShow": true,
+            "defaultWidth": 800,
+            "defaultHeight": 600
+        },
+        "runtime": {
+            "arguments": "",
+            "version": "stable"
+        },
+        "shortcut": {
+            "company": "Eikos Partners",
+            "description": "Latency Measurements of a simple Node.js project",
+            "icon": "http://" + host + "/images/eikos-logo-multi.ico",
+            "name": "Node Performance Measure"
+        }
+    };
+    return json;
+}
+
+module.exports = createJson;
