2ff2f08b762ee3e255eb44eb1d0174310c8a1733
zdonato <zdonato@stevens.edu>
2017-09-25 12:56:22 -0400

    Adds a layout page as an example of using the windowmanager layout object.

---
+++ public/js/layout.js
@@ -0,0 +1,15 @@
+let state = {
+    width: 400, 
+    height: 400,
+    url: 'child.html',
+    title: 'Window',
+    frame: false 
+};
+
+let configs = [state, state, state, state, state]
+
+let layout = new windowmanager.Layout('tile', 'layout-example', configs); 
+
+function addWindow() {
+    layout.addWindow(state);
+}
