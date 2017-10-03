dc5491a6419bb691b010a132b2a76f6365236813
Zachary Donato <zdonato@stevens.edu>
2017-10-02 21:23:49 -0400

    Shows example of tabbed layout.

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,15 +1,49 @@
-let state = {
+let state1 = {
     width: 400, 
     height: 400,
     url: 'child.html',
-    title: 'Window' //+ windowmanager.Window.getAll().length,
+    title: 'Window 1',
+    frame: false 
+};
+let state2 = {
+    width: 400, 
+    height: 400,
+    url: 'child.html',
+    title: 'Window 2',
+    frame: false 
+};
+let state3 = {
+    width: 400, 
+    height: 400,
+    url: 'child.html',
+    title: 'Window 3',
+    frame: false 
+};
+let state4 = {
+    width: 400, 
+    height: 400,
+    url: 'child.html',
+    title: 'Window 4',
+    frame: false 
+};
+let state5 = {
+    width: 400, 
+    height: 400,
+    url: 'child.html',
+    title: 'Window 5',
     frame: false 
 };
 
-let configs = [state, state, state, state, state]
+let configs = [state1, state2, state3, state4, state5];
 
-let layout = new windowmanager.Layout('tile', 'layout-example', configs); 
+let layout = new windowmanager.Layout('tabbed', 'layout-example', configs); 
 
 function addWindow() {
-    layout.addWindow(state);
+    layout.addWindow({
+        width: 400, 
+        height: 400,
+        url: 'child.html',
+        title: 'Window 6',
+        frame: false 
+    });
 }
